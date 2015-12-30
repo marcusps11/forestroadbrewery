dc498d51678a4908f89c41695bec5790e293a81f
marcusps11 <marcus@1roof.com>
2015-12-30 15:47:30 +0000

    favicon

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 var router = express.Router();
 var nodemailer = require('nodemailer');
+var favicon = require('serve-favicon');
 var Beer = require('./models/beer');
 
 var port = process.env.PORT || 3000; 
@@ -16,6 +17,8 @@ var databaseURL = process.env.MONGOLAB_URI || 'mongodb://localhost/forestroad';
 mongoose.connect(databaseURL); 
 
 app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/public/img/favicon.ico'));
+app.use(favicon(path.join(__dirname,'public','img','favicon.ico')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
